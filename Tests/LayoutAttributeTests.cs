53826c08b00c4d5672f71528df1369f1745774ff
Joni Savolainen <joni@savolainen.io>
2022-05-01 12:42:05 +0300

    test: create new fixture for ResourcesAssetResolver tests

--- Tests/LayoutAttributeTests.cs
+++ Tests/LayoutAttributeTests.cs
@@ -1,5 +1,7 @@
-﻿using NUnit.Framework;
+﻿using NSubstitute;
+using NUnit.Framework;
 using UIComponents.Core;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace UIComponents.Tests
@@ -7,20 +9,29 @@ namespace UIComponents.Tests
     [TestFixture]
     public class LayoutAttributeTests
     {
-        [Layout("UIComponentTests/LayoutAttributeTests")]
+        [Layout("Assets/MyAsset.uxml")]
         private class UIComponentWithLayout : UIComponent {}
+
+        private IAssetResolver _assetResolver;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _assetResolver = Substitute.For<IAssetResolver>();
+            DependencyInjector.SetDependency<UIComponentWithLayout, IAssetResolver>(
+                _assetResolver
+            );
+        }
 
         [Test]
         public void Given_Layout_Is_Loaded()
         {
-            var component = new UIComponentWithLayout();
+            _assetResolver.LoadAsset<VisualTreeAsset>("Assets/MyAsset.uxml")
+                .Returns(ScriptableObject.CreateInstance<VisualTreeAsset>());
             
-            Assert.That(component.childCount, Is.EqualTo(1));
+            var component = new UIComponentWithLayout();
 
-            var label = component.Q<Label>();
-
-            Assert.That(label, Is.Not.Null);
-            Assert.That(label.text, Is.EqualTo("Hello world!"));
+            _assetResolver.Received().LoadAsset<VisualTreeAsset>("Assets/MyAsset.uxml");
         }
     }
 }
---
+++ Tests/ResourcesAssetResolverTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using UIComponents.Core;
+using UnityEngine.UIElements;
+
+namespace UIComponents.Tests
+{
+    [TestFixture]
+    public class ResourcesAssetResolverTests
+    {
+        [Test]
+        public void Should_Be_Able_To_Load_Existing_Asset()
+        {
+            var assetResolver = new ResourcesAssetResolver();
+
+            var layout = assetResolver.LoadAsset<VisualTreeAsset>(
+                "UIComponentTests/LayoutAttributeTests"
+            );
+
+            var visualElement = new VisualElement();
+            layout.CloneTree(visualElement);
+
+            Assert.That(visualElement.childCount, Is.EqualTo(1));
+
+            var label = visualElement.Q<Label>();
+
+            Assert.That(label, Is.Not.Null);
+            Assert.That(label.text, Is.EqualTo("Hello world!"));
+        }
+
+        [Test]
+        public void Should_Be_Able_To_Tell_If_Asset_Exists()
+        {
+            var assetResolver = new ResourcesAssetResolver();
+
+            var existingAssetExists =
+                assetResolver.AssetExists("UIComponentTests/LayoutAttributeTests");
+            var nonExistantAssetDoesNotExist =
+                assetResolver.AssetExists("SomeOtherAsset");
+            
+            Assert.That(existingAssetExists, Is.True);
+            Assert.That(nonExistantAssetDoesNotExist, Is.False);
+        }
+    }
+}
