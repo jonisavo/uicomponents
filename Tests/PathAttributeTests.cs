762f8dbcd0ee86b38593c0ecd8f5f3e2208b9492
Joni Savolainen <joni@savolainen.io>
2022-05-01 11:23:59 +0300

    test: add tests for PathAttribute

---
+++ Tests/PathAttributeTests.cs
@@ -0,0 +1,83 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using UIComponents.Core;
+
+namespace UIComponents.Tests
+{
+    [TestFixture]
+    public class PathAttributeTests
+    {
+        private class BasicPathAttribute : PathAttribute
+        {
+            public BasicPathAttribute(string path)
+            {
+                Path = path;
+            }
+        }
+        
+        [TestFixture]
+        public class GetAssetPathForComponent
+        {
+            private class UIComponentWithNoAssetPaths : UIComponent {}
+            
+            [AssetPath("Assets/Valid/Path")]
+            private class UIComponentWithValidAssetPath : UIComponent {}
+            
+            [AssetPath("Assets/Invalid/Path")]
+            private class UIComponentWithInvalidAssetPath : UIComponent {}
+
+            private IAssetResolver _assetResolver;
+
+            [OneTimeSetUp]
+            public void OneTimeSetUp()
+            {
+                _assetResolver = Substitute.For<IAssetResolver>();
+                DependencyInjector.SetDependency<UIComponentWithValidAssetPath, IAssetResolver>(_assetResolver);
+                DependencyInjector.SetDependency<UIComponentWithInvalidAssetPath, IAssetResolver>(_assetResolver);
+            }
+
+            [Test]
+            public void Should_Return_Empty_String_If_No_Path_Is_Configured()
+            {
+                var pathAttribute = new BasicPathAttribute(null);
+                var component = new UIComponentWithNoAssetPaths();
+                
+                var path = pathAttribute.GetAssetPathForComponent(component);
+                
+                Assert.That(path, Is.EqualTo(""));
+            }
+
+            [Test]
+            public void Should_Ignore_AssetPath_In_Case_Of_Complete_Path()
+            {
+                var assetsPathAttribute = new BasicPathAttribute("Assets/Asset");
+                var packagesPathAttribute = new BasicPathAttribute("Packages/Asset");
+                var component = new UIComponentWithValidAssetPath();
+
+                var assetPath = assetsPathAttribute.GetAssetPathForComponent(component);
+                var packagePath = packagesPathAttribute.GetAssetPathForComponent(component);
+                
+                Assert.That(assetPath, Is.EqualTo("Assets/Asset"));
+                Assert.That(packagePath, Is.EqualTo("Packages/Asset"));
+            }
+
+            [Test]
+            public void Should_Use_Valid_AssetPath_In_Case_Of_Incomplete_Path()
+            {
+                var pathAttribute = new BasicPathAttribute("MyAsset");
+                
+                _assetResolver.AssetExists("Assets/Valid/Path/MyAsset").Returns(true);
+                _assetResolver.AssetExists("Assets/Invalid/Path/MyAsset").Returns(false);
+
+                var componentWithValidPath = new UIComponentWithValidAssetPath();
+                var componentWithInvalidPath = new UIComponentWithInvalidAssetPath();
+
+                var filledPath = pathAttribute.GetAssetPathForComponent(componentWithValidPath);
+                var ignoredPath = pathAttribute.GetAssetPathForComponent(componentWithInvalidPath);
+                
+                Assert.That(filledPath, Is.EqualTo("Assets/Valid/Path/MyAsset"));
+                Assert.That(ignoredPath, Is.EqualTo("MyAsset"));
+            }
+        } 
+    }
+}
