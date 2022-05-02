423c59305953eefe1eaa62e5ff79249f33bab842
Joni Savolainen <joni@savolainen.io>
2022-05-02 09:23:42 +0300

    fix: remove visibility modifier from interface members
    They cause a compilation error on Unity 2019.

--- Core/IAssetResolver.cs
+++ Core/IAssetResolver.cs
@@ -12,13 +12,13 @@
         /// <param name="assetPath">Asset path</param>
         /// <typeparam name="T">Asset type</typeparam>
         /// <returns>Asset object</returns>
-        public T LoadAsset<T>(string assetPath) where T : UnityEngine.Object;
+        T LoadAsset<T>(string assetPath) where T : UnityEngine.Object;
 
         /// <summary>
         /// Returns whether the asset exists.
         /// </summary>
         /// <param name="assetPath">Asset path</param>
         /// <returns>Whether the asset exists</returns>
-        public bool AssetExists(string assetPath);
+        bool AssetExists(string assetPath);
     }
 }
