2a4f3357f089d9ad3da5c4e968b6f222122166b1
Joni Savolainen <joni@savolainen.io>
2022-05-05 08:01:51 +0300

    chore(project): add addressables example

---
+++ Assets/Examples/Addressables/AddressablesExampleComponent.cs
@@ -0,0 +1,11 @@
+﻿using UIComponents.Core;
+using UIComponents.Addressables;
+
+namespace UIComponentsExamples.Addressables
+{
+    [AssetPath("Assets/Examples/Addressables/Data")]
+    [Layout("AddressablesExampleComponent.uxml")]
+    [Stylesheet("AddressablesExampleComponent.uss")]
+    [Dependency(typeof(IAssetResolver), provide: typeof(AddressableAssetResolver))]
+    public class AddressablesExampleComponent : UIComponent {}
+}
---
+++ Assets/Examples/Addressables/Editor/AddressablesExampleComponentWindow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UIComponentsExamples.Addressables.Editor
+{
+    public class AddressablesExampleComponentWindow : EditorWindow
+    {
+        [MenuItem("UIComponents Examples/Loading Assets/Addressables")]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<AddressablesExampleComponentWindow>();
+            window.titleContent = new GUIContent("Loading from Addressables");
+            window.Show();
+        }
+
+        private void CreateGUI()
+        {
+            rootVisualElement.style.minWidth = 150;
+            rootVisualElement.style.minHeight = 40;
+            rootVisualElement.Add(new AddressablesExampleComponent());
+        }
+    }
+}
