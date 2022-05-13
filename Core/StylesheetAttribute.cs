c6cc37ecbc8133801561a1d5aaeaef30e103452c
Joni Savolainen <joni@savolainen.io>
2022-05-13 20:15:55 +0300

    test: add tests for LayoutAttribute and StylesheetAttribute inheritance

--- Core/StylesheetAttribute.cs
+++ Core/StylesheetAttribute.cs
@@ -5,8 +5,8 @@ namespace UIComponents
 {
     /// <summary>
     /// Specifies the path to a .uss stylesheet file used by a UIComponent.
-    /// <seealso cref="AssetPathAttribute"/>
     /// </summary>
+    /// <seealso cref="AssetPathAttribute"/>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
     [BaseTypeRequired(typeof(UIComponent))]
     public class StylesheetAttribute : PathAttribute
