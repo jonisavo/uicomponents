e4ad7242e7be66f246f49566a4c989b65777a664
Joni Savolainen <joni@savolainen.io>
2022-06-10 19:15:07 +0300

    fix(QueryAttribute): fix query by type on Unity 2019

---
+++ Assets/UIComponents/Core/Internal/TypeUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UIComponents.Internal
+{
+    /// <summary>
+    /// Internal utilities for types.
+    /// </summary>
+    internal static class TypeUtils
+    {
+        /// <summary>
+        /// Returns a concrete type that is not an array or List.
+        /// </summary>
+        /// <param name="type">Array, List or concrete type</param>
+        /// <returns>Concrete type</returns>
+        public static Type GetConcreteType(Type type)
+        {
+            if (type.IsArray)
+                return type.GetElementType();
+
+            if (CollectionUtils.TypeQualifiesAsList(type))
+                return type.GenericTypeArguments[0];
+
+            return type;
+        }
+    }
+}
