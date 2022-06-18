b8a089c8690da5d23381496a66d4fcbcac3f20ce
Joni Savolainen <joni@savolainen.io>
2022-06-18 08:46:12 +0300

    feat(UIComponent): add GetTypeName method

---
+++ Assets/UIComponents.Tests/UIComponentTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace UIComponents.Tests
+{
+    [TestFixture]
+    public class UIComponentTests
+    {
+        [TestFixture]
+        public class GetTypeName
+        {
+            private class TestComponent : UIComponent {}
+            
+            [Test]
+            public void ShouldReturnTypeName()
+            {
+                var component = new TestComponent();
+                Assert.That(nameof(TestComponent), Is.EqualTo(component.GetTypeName()));
+            }
+        }
+    }
+}
