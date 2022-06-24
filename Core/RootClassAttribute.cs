daca5249fabf2c884dc335c47bc55060bb338892
Joni Savolainen <joni@savolainen.io>
2022-06-24 14:25:49 +0300

    feat: add RootClassAttribute

---
+++ Core/RootClassAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UIComponents
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class RootClassAttribute : UIComponentEffectAttribute
+    {
+        private readonly string _className;
+        
+        public RootClassAttribute(string className)
+        {
+            _className = className;
+        }
+        
+        public override void Apply(UIComponent component)
+        {
+            component.AddToClassList(_className);
+        }
+    }
+}
---
+++ Core/UIComponentEffectAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace UIComponents
+{
+    /// <summary>
+    /// An attribute used to apply effects to a <see cref="UIComponent"/>.
+    /// They are applied after the layout and assets have been loaded.
+    /// Attributes with higher priority are applied first.
+    /// </summary>
+    [BaseTypeRequired(typeof(UIComponent))]
+    public abstract class UIComponentEffectAttribute : Attribute, IComparable<UIComponentEffectAttribute>
+    {
+        public int Priority { get; set; } = 0;
+        
+        public abstract void Apply(UIComponent component);
+        
+        public int CompareTo(UIComponentEffectAttribute other)
+        {
+            return Priority.CompareTo(other.Priority);
+        }
+    }
+}
