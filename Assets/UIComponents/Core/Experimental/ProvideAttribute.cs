0c54ea0d74d962462adcadfbbafc1357a0e9d67b
Joni Savolainen <joni@savolainen.io>
2022-06-24 10:16:47 +0300

    feat: add experimental ProvideAttribute

---
+++ Assets/UIComponents/Core/Experimental/ProvideAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace UIComponents.Experimental
+{
+    /// <summary>
+    /// An attribute for specifying a field to be automatically
+    /// assigned using dependency injection in UIComponents.
+    /// If no provider exists for the dependency, the field
+    /// is left unassigned and an error is logged.
+    /// </summary>
+    /// <example>
+    /// [Dependency(typeof(ILogger), provide: typeof(MyLogger)]
+    /// [Dependency(typeof(IDataService), provide: typeof(DataService)]
+    /// public class ComponentWithDependencies : UIComponent
+    /// {
+    ///     [Provide]
+    ///     private readonly ILogger Logger;
+    ///
+    ///     [Provide]
+    ///     private readonly IDataService DataService;
+    /// }
+    /// </example>
+    /// <seealso cref="DependencyAttribute"/>
+    [AttributeUsage(AttributeTargets.Field)]
+    [MeansImplicitUse]
+    public class ProvideAttribute : Attribute
+    {
+        
+    }
+}
