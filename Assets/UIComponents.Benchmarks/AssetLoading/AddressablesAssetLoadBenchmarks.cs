a7e4ccc7fad871e4a0779a139dadda3fba795e84
Joni Savolainen <joni@savolainen.io>
2022-08-20 19:10:48 +0300

    test(benchmarks): ensure Addressables are initialized before running benchmarks

--- Assets/UIComponents.Benchmarks/AssetLoading/AddressablesAssetLoadBenchmarks.cs
+++ Assets/UIComponents.Benchmarks/AssetLoading/AddressablesAssetLoadBenchmarks.cs
@@ -1,6 +1,8 @@
-﻿using NUnit.Framework;
+﻿using System.Collections;
+using NUnit.Framework;
 using UIComponents.Addressables;
 using Unity.PerformanceTesting;
+using UnityEngine.TestTools;
 
 namespace UIComponents.Benchmarks.AssetLoading
 {
@@ -13,6 +15,15 @@ namespace UIComponents.Benchmarks.AssetLoading
         [Stylesheet("Box.uss")]
         [Dependency(typeof(IAssetResolver), provide: typeof(AddressableAssetResolver), Scope.Transient)]
         private class ComponentWithAssets : UIComponent {}
+        
+        [UnitySetUp]
+        public IEnumerator UnitySetUp()
+        {
+            var initOperation = UnityEngine.AddressableAssets.Addressables.InitializeAsync();
+
+            while (!initOperation.IsDone)
+                yield return null;
+        }
 
         [Test, Performance, Version(BenchmarkUtils.Version)]
         public void InitializeComponentWithWarmCache()
