7f055a07d7af2220506d698bdef67da76342411a
Joni Savolainen <joni@savolainen.io>
2022-08-20 19:26:09 +0300

    Revert "test(benchmarks): ensure Addressables are initialized before running benchmarks"
    This reverts commit a7e4ccc7fad871e4a0779a139dadda3fba795e84.

--- Assets/UIComponents.Benchmarks/AssetLoading/AddressablesAssetLoadBenchmarks.cs
+++ Assets/UIComponents.Benchmarks/AssetLoading/AddressablesAssetLoadBenchmarks.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using UIComponents.Addressables;
 using Unity.PerformanceTesting;
-using UnityEngine.TestTools;
 
 namespace UIComponents.Benchmarks.AssetLoading
 {
@@ -15,15 +13,6 @@ namespace UIComponents.Benchmarks.AssetLoading
         [Stylesheet("Box.uss")]
         [Dependency(typeof(IAssetResolver), provide: typeof(AddressableAssetResolver), Scope.Transient)]
         private class ComponentWithAssets : UIComponent {}
-        
-        [UnitySetUp]
-        public IEnumerator UnitySetUp()
-        {
-            var initOperation = UnityEngine.AddressableAssets.Addressables.InitializeAsync();
-
-            while (!initOperation.IsDone)
-                yield return null;
-        }
 
         [Test, Performance, Version(BenchmarkUtils.Version)]
         public void InitializeComponentWithWarmCache()
