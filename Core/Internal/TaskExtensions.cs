121125186fa00f733a200660c59873273acd92dd
Joni Savolainen <joni@savolainen.io>
2022-08-28 11:21:32 +0300

    feat(UIComponent): add WaitForInitializationEnumerator method

---
+++ Core/Internal/TaskExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Runtime.ExceptionServices;
+using System.Threading.Tasks;
+using UnityEngine.TestTools;
+
+namespace UIComponents.Internal
+{
+    [ExcludeFromCoverage]
+    internal static class TaskExtensions
+    {
+        public static IEnumerator AsEnumerator(this Task task)
+        {
+            while (!task.IsCompleted)
+            {
+                yield return null;
+            }
+
+            if (task.IsFaulted)
+            {
+                ExceptionDispatchInfo.Capture(task.Exception).Throw();
+            }
+
+            yield return null;
+        }
+    }
+}
