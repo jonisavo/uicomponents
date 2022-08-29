ad84e67138d7db3f0ec375250d338da0231f12c0
Joni Savolainen <joni@savolainen.io>
2022-08-29 07:47:26 +0300

    docs(IUIComponentLogger): add documentation

--- Assets/UIComponents/Core/IUIComponentLogger.cs
+++ Assets/UIComponents/Core/IUIComponentLogger.cs
@@ -1,11 +1,29 @@
 ﻿namespace UIComponents
 {
+    /// <summary>
+    /// An interface for logging information in UIComponents.
+    /// </summary>
     public interface IUIComponentLogger
     {
+        /// <summary>
+        /// Logs a regular message.
+        /// </summary>
+        /// <param name="message">Message to log</param>
+        /// <param name="component">UIComponent which logged this message</param>
         void Log(string message, UIComponent component);
 
+        /// <summary>
+        /// Logs a warning.
+        /// </summary>
+        /// <param name="message">Message to log</param>
+        /// <param name="component">UIComponent which logged this message</param>
         void LogWarning(string message, UIComponent component);
         
+        /// <summary>
+        /// Logs an error.
+        /// </summary>
+        /// <param name="message">Message to log</param>
+        /// <param name="component">UIComponent which logged this message</param>
         void LogError(string message, UIComponent component);
     }
 }
