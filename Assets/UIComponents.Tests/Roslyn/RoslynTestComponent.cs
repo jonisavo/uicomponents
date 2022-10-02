1d32adf79c94d177b719be31079f82dce8da8074
Joni Savolainen <joni@savolainen.io>
2022-10-02 08:58:40 +0300

    test(roslyn): add integration tests

---
+++ Assets/UIComponents.Tests/Roslyn/RoslynTestComponent.cs
@@ -0,0 +1,27 @@
+﻿using UIComponents.Experimental;
+using UnityEngine;
+
+namespace UIComponents.Tests.Roslyn
+{
+    public partial class RoslynTestComponent : UIComponent
+    {
+        public enum Greetings
+        {
+            Hello,
+            Hi,
+            Morning
+        }
+
+        [Trait(Name = "text-color")]
+        public Color TextColor;
+
+        [Trait(Name = "current-time")]
+        public double CurrentTime;
+
+        [Trait(DefaultValue = Greetings.Morning)]
+        public Greetings Greeting;
+
+        [Trait(DefaultValue = true)]
+        public bool Enabled;
+    }
+}
