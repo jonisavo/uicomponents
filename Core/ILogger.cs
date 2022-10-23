c1cf436eeda98fbbfa0b6175bb41da1acae0db69
Joni Savolainen <joni@savolainen.io>
2022-10-23 08:58:53 +0300

    feat: rename IUIComponentLogger to ILogger, turn caller argument type to object
    BREAKING CHANGE: The IUIComponentLogger interface has been renamed to ILogger. It's methods' second argument is now of type object rather than UIComponent.

--- Core/ILogger.cs
+++ Core/ILogger.cs
@@ -3,27 +3,27 @@
     /// <summary>
     /// An interface for logging information in UIComponents.
     /// </summary>
-    public interface IUIComponentLogger
+    public interface ILogger
     {
         /// <summary>
         /// Logs a regular message.
         /// </summary>
         /// <param name="message">Message to log</param>
-        /// <param name="component">UIComponent which logged this message</param>
-        void Log(string message, UIComponent component);
+        /// <param name="caller">Object which logged this message</param>
+        void Log(string message, object caller);
 
         /// <summary>
         /// Logs a warning.
         /// </summary>
         /// <param name="message">Message to log</param>
-        /// <param name="component">UIComponent which logged this message</param>
-        void LogWarning(string message, UIComponent component);
+        /// <param name="caller">Object which logged this message</param>
+        void LogWarning(string message, object caller);
         
         /// <summary>
         /// Logs an error.
         /// </summary>
         /// <param name="message">Message to log</param>
-        /// <param name="component">UIComponent which logged this message</param>
-        void LogError(string message, UIComponent component);
+        /// <param name="caller">Object which logged this message</param>
+        void LogError(string message, object caller);
     }
 }
