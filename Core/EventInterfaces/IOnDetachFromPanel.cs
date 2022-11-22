379798382ad2ae9361923f16b6b02911f368e96d
Joni Savolainen <joni@savolainen.io>
2022-11-22 20:40:06 +0200

    feat: move event interfaces to use source generation
    Introduces a new RegistersEventCallbackAttribute which can be used on interfaces to register event callbacks.

--- Core/EventInterfaces/IOnDetachFromPanel.cs
+++ Core/EventInterfaces/IOnDetachFromPanel.cs
@@ -1,4 +1,5 @@
 ﻿using JetBrains.Annotations;
+using UIComponents.InterfaceModifiers;
 using UnityEngine.UIElements;
 
 namespace UIComponents
@@ -8,6 +9,7 @@ namespace UIComponents
     /// a callback for <see cref="DetachFromPanelEvent"/> is
     /// automatically registered in the UIComponent constructor.
     /// </summary>
+    [RegistersEventCallback(typeof(DetachFromPanelEvent))]
     public interface IOnDetachFromPanel
     {
         void OnDetachFromPanel([NotNull] DetachFromPanelEvent evt);
