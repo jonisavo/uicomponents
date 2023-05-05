5bb36262e51f30dbc90764664d62a06c58a7aefa
Joni Savolainen <joni@savolainen.io>
2023-05-05 19:45:37 +0300

    refactor: rename private members in QueryExampleComponent

--- Assets/Samples/Query/QueryExampleComponent.cs
+++ Assets/Samples/Query/QueryExampleComponent.cs
@@ -8,26 +8,26 @@ namespace UIComponents.Samples.Query
     public partial class QueryExampleComponent : UIComponent
     {
         [Query("my-label")]
-        private Label MyLabel;
+        private Label _myLabel;
     
         [Query(Name = "my-foldout")]
-        private Foldout MyFoldout;
+        private Foldout _myFoldout;
 
         [Query(Class = "description")]
-        private Label[] DescriptionLabels;
+        private Label[] _descriptionLabels;
 
         [Query]
-        private VisualElement[] Everything;
+        private VisualElement[] _everything;
 
         public override void OnInit()
         {
-            MyLabel.text = "This text was set in the component's OnInit function.";
-            MyFoldout.Add(new Label("This label was added in the component's OnInit function."));
+            _myLabel.text = "This text was set in the component's OnInit function.";
+            _myFoldout.Add(new Label("This label was added in the component's OnInit function."));
 
-            foreach (var label in DescriptionLabels)
+            foreach (var label in _descriptionLabels)
                 label.style.color = Color.green;
 
-            foreach (var element in Everything)
+            foreach (var element in _everything)
                 element.tooltip = "Everything has this tooltip.";
         }
     }
