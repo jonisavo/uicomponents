57153a8c90d17344773ee86e45e5f89095dc66c8
Joni Savolainen <joni@savolainen.io>
2023-05-26 18:42:33 +0300

    feat: add AssetDatabaseAssetResolver

---
+++ Assets/UIComponents.Benchmarks/AssetLoading/AssetDatabaseAssetLoadBenchmarks.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using UIComponents.Editor;
+using Unity.PerformanceTesting;
+
+namespace UIComponents.Benchmarks.AssetLoading
+{
+    [TestFixture]
+    public partial class AssetDatabaseAssetLoadBenchmarks
+    {
+        [AssetPrefix("Assets/Samples/Addressables/Data/")]
+        [Layout("AddressablesExampleComponent.uxml")]
+        [Stylesheet("AddressablesExampleComponent.uss")]
+        [Stylesheet("Box.uss")]
+        [Dependency(typeof(IAssetResolver), provide: typeof(AssetDatabaseAssetResolver), Scope.Transient)]
+        private partial class ComponentWithAssets : UIComponent {}
+
+        [Test, Performance, Version(BenchmarkUtils.Version)]
+        public void InitializeComponentWithWarmCache()
+        {
+            BenchmarkUtils.MeasureComponentInitWithWarmCache<ComponentWithAssets>();
+        }
+        
+        [Test, Performance, Version(BenchmarkUtils.Version)]
+        public void InitializeComponentWithColdCache()
+        {
+            BenchmarkUtils.MeasureComponentInitWithColdCache<ComponentWithAssets>();
+        }
+    }
+}
